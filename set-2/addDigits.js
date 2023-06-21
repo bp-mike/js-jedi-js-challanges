532acbb4b9e9f6006f55edd74e5cc69c9b30e79e
Ainerukundo Edwins <ainedwins@gmail.com>
2023-06-21 11:21:38 +0300

    My solution

--- set-2/addDigits.js
+++ set-2/addDigits.js
@@ -12,3 +12,27 @@ function addDigits(num) { // challenge 1
 let num = 12345;
 let result = addDigits(num);
 console.log(result);
+
+
+//My Solution
+//lets the user input any number of choice
+let x = prompt("Input number: ");
+
+function separate(x) {
+  let y = x.toString(); //converts it to string so separation is easier
+	
+  let sum = 0;
+  let z = 0;
+  for (z; z < y.length; z++) {
+    sum = sum + parseInt(y[z]);
+  }
+  
+  if (sum < 10) {
+    return sum;
+  } else {
+    return separate(sum);
+  }
+}
+
+var results = separate(x);
+console.log(results);
