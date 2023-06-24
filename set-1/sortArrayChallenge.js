d539ff622e4869303ae51399122226d95f98db95
bpmike <bpmikeomo@gmail.com>
2023-06-25 02:38:24 +0300

    bubbleSort the arrays

--- set-1/sortArrayChallenge.js
+++ set-1/sortArrayChallenge.js
@@ -1,62 +1,61 @@
-function sortArray(arr){ // challenge 5
-    // // Initialize empty arrays for evens, odds, and chars
-    // let evens = []
-    // let odds = []
-    // let chars = []
-
-    // // Iterate through each element in the input array
-    // // for each element in arr
-    //     // if element is an even number
-    //         // append element to evens
-    //     // else if element is an odd number
-    //         // append element to odds
-    //     // else if element is a single-character string
-    //         // append element to chars
-    
-    // // Sort the arrays in ascending order
-    // // sort evens
-    // // sort odds
-    // // sort chars
-    // // N.B - i suggest write a function to sort that can be re-used 3 times for sorting evens, odds and chars
-    
-    // // Create and return the result object
-    // let result = {
-    //     evens: evens,
-    //     odds: odds,
-    //     chars: chars
-    // }
-    // return result
-
-    let evens = [];
+function sortArray(arr) {
+  // challenge 5
+  // Initialize empty arrays for evens, odds, and chars
+  let evens = [];
   let odds = [];
   let chars = [];
 
+  // Iterate through each element in the input array
   for (let item of arr) {
-    if (typeof item === 'number' && Number.isInteger(item)) {
+    if (typeof item === "number" && Number.isInteger(item)) {
       if (item % 2 === 0) {
         evens.push(item);
       } else {
         odds.push(item);
       }
-    } else if (typeof item === 'string' && item.length === 1) {
+    } else if (typeof item === "string" && item.length === 1) {
       chars.push(item);
     }
   }
-
-  evens.sort((a, b) => a - b);
-  odds.sort((a, b) => a - b);
-  chars.sort();
-
+  // Sort the arrays in ascending order
+  evens = bubbleSort(evens);
+  odds = bubbleSort(odds);
+  chars = bubbleSort(chars);
+  // Create and return the result object
   const result = {
     evens: evens,
     odds: odds,
-    chars: chars
+    chars: chars,
   };
 
   return result;
 }
 
+// bubbleSort function thhat takes an array
+function bubbleSort(arr) {
+  const length = arr.length;
+  // initializes  swapped to true, it keeps track of whether any swaps were made during the sorting process
+  let swapped = true;
+
+  while (swapped) {
+    // resets swapped to false at the start of each iteration, assuming no swaps will be made unless proven otherwise.
+    swapped = false;
+
+    //   this loop compares adjacent arr and swapps them if they are in the wrong order.
+    for (let i = 0; i < length - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        let swap = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = swap;
+        swapped = true;
+      }
+    }
+  }
+
+  return arr;
+}
+
 // Example usage:
-let inputArray = [1, 3, 5, 2, 4, 6, 9, 8, 'a', 'c', 'b']
-let result = sortArray(inputArray)
-console.log(result)
+let inputArray = [1, 3, 5, 2, 4, 6, 9, 8, "a", "c", "b"];
+let result = sortArray(inputArray);
+console.log(result);
