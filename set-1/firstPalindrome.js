35d385a3d78c92b2ffecda66f594971c4d09f425
bpmike <bpmikeomo@gmail.com>
2023-06-26 23:13:59 +0300

    first Palindrome solution

--- set-1/firstPalindrome.js
+++ set-1/firstPalindrome.js
@@ -1,18 +1,33 @@
-function firstPalindrome(words){ // challenge 4
-    // for each word in words:
-        // if word is a palindrome:(use the isPalindrome below to check if the word is a palindrome)
-            // return word
-    
-    // No palindromic string found
-    // return empty string
+function firstPalindrome(words) {
+  // challenge 4
+  // Iterate over each word in the  array
+  for (let word of words) {
+    // Check if the word is a palindrome using the isPalindrome below function
+    if (isPalindrome(word)) {
+      // If the word is a palindrome, return it as the first palindrome found
+      return word;
+    }
+  }
+  // If no palindrome is found, return an empty string
+  return "";
 }
 
 function isPalindrome(word) {
-    const reversed = word.split('').reverse().join('');
-    return word === reversed;
+  // Reverse the word by splitting it into an array of characters,
+  // reversing the order, and joining them back into a string
+  const reversed = word.split("").reverse().join("");
+  // return true if the original word is equal to the reversed word, false otherwise
+  return word === reversed;
 }
 
 // Example usage:
-let words = ["level", "hello", "racecar", "world"]
-let result = firstPalindrome(words)
-console.log(result)
+let words = [
+  // "ada",
+  "cool",
+  //  "level",
+  "hello",
+  "racecar",
+  "world",
+];
+let result = firstPalindrome(words);
+console.log(result);
